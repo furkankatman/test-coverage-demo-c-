38d51f0510e06bab4496e8e8b67b4ab526b7d9e0
furkankatman <furkan.katman@gmail.com>
2026-02-18 13:00:08 +0100

    first commit

---
+++ Vehicle.cs
@@ -0,0 +1,24 @@
+public class Vehicle
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public Vehicle(string brand, string model, int year)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+    }
+    public void Start()
+    {
+        Console.WriteLine("Vehicle is starting...");
+        // Logic to start the vehicle
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Vehicle is stopping...");
+        // Logic to stop the vehicle
+    }
+}
