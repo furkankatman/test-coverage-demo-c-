c21e26f89d6daff452dc8f87acd023b41a6778ca
furkankatman <furkan.katman@gmail.com>
2026-02-18 15:03:19 +0100

    added a test case for start method

---
+++ test-coverage-demo-c-.Tests/VehicleTests.cs
@@ -0,0 +1,27 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace test_coverage_demo_c_.Tests
+{
+    [TestClass]
+    public class VehicleTests
+    {
+        [TestMethod]
+        public void Start_ShouldWriteExpectedMessage()
+        {
+            // Arrange
+            var vehicle = new Vehicle("Toyota", "Camry", 2020);
+            using var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Act
+            vehicle.Start();
+
+            // Assert
+            var output = consoleOutput.ToString().Trim();
+            Assert.AreEqual("Vehicle is starting...", output);
+        }
+    }
+}
